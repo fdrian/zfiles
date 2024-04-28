27d7cd5b3f440bb2415649b60ef02cc59fee9375
Drian <11167300+fdrian@users.noreply.github.com>
2024-04-28 03:35:56 -0300

    fixes

--- setup/essential.sh
+++ setup/essential.sh
@@ -7,20 +7,19 @@ echo "${YELLOW}[+] Running $0...${RESET}"
 
 # Check system version
 if [[ $ID == "arch" || $ID_LIKE == *"arch"* ]]; then
-    if command -v yay &> /dev/null; then 
+    if command -v yay &>/dev/null; then 
         while read -r package; do    
             yay -S --noconfirm --needed  $package || echo "${RED}$(date) -  Error installing $package${RESET}" >> $LOG_FILE
         done < "$PACKAGE_LIST"
     else
         echo "${RED}yay is not installed. Please install yay first.${RESET}"
     fi
-elif [[ $ID == "debian" || $ID_LIKE == *"debian"* ]]; then
+elif [[ $ID == "ubuntu" || $ID_LIKE == *"debian"* ]]; then
     while read -r package; do    
         apt install -y $package || echo "${RED}$(date) -  Error installing $package${RESET}" >> $LOG_FILE
     done < "$PACKAGE_LIST"    
 else
-    echo "Distribution not supported for automatic package installation."
+    echo "${RED}Distribution not supported for automatic package installation!${RESET}"
 fi
 
-echo "${GREEN} Finished...${RESET}"
 sleep 1
