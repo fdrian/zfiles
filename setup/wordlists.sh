48c7fa4db4161260356407f202d813d46d5a1104
Drian <11167300+fdrian@users.noreply.github.com>
2024-05-08 04:01:40 -0300

    fixes

--- setup/wordlists.sh
+++ setup/wordlists.sh
@@ -4,7 +4,7 @@
 echo "${YELLOW}[+] Running $0...${RESET}"
 
 if [[ ! -d /usr/share/wordlists ]]; then
-    mkdir -p /usr/share/wordlits
+    mkdir -p /usr/share/wordlists
 
     wget -q -c --show-progress https://github.com/danielmiessler/SecLists/archive/master.zip -O /tmp/SecList.zip
     7z x /tmp/SecList.zip -o/usr/share/wordlists
