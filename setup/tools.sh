206917646ecf8ee67012c5f947959ff9233262c3
Drian <11167300+fdrian@users.noreply.github.com>
2024-05-08 03:11:17 -0300

    check if tool is installed

--- setup/tools.sh
+++ setup/tools.sh
@@ -6,13 +6,18 @@ echo "${YELLOW}[+] Running $0...${RESET}"
 install_tool() {
   local tool="$1"
   local repo="$2"
-  echo "Installing $tool"
-  sleep 1
-  go install $repo@latest | pv
+
+  # Check if tool is installed
+  if command -v $tool &>/dev/null; then
+    echo "${YELLOW}[+] $tool is already installed${RESET}"
+  else
+    echo "${BLUE}[+] Installing $tool${RESET}"
+    sleep 1
+    go install $repo@latest | pv
+  fi
 }
 
 TOOLS_LIST="$ZFILES/setup/packages/tools.txt" 
-
 
 while read -r tool repo; do    
     install_tool  $tool $repo
