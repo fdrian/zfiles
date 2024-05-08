7fc3e78d64d054a4ea37b92d2fcb2b39c739dd71
Drian <11167300+fdrian@users.noreply.github.com>
2024-05-08 03:38:22 -0300

    fixes

--- setup/essential.sh
+++ setup/essential.sh
@@ -10,15 +10,25 @@ if [[ $ID == "arch" || $ID_LIKE == *"arch"* ]]; then
     sudo pacman -Syu
     if command -v yay &>/dev/null; then 
         while read -r package; do    
-            yay -S --noconfirm --needed  $package 2>> $LOG_FILE
+            if command -v $package &>/dev/null;then
+                echo "${YELLOW}[+] $package is already installed!${RESET}"
+            else
+                yay -S --noconfirm --needed  $package 2>> $LOG_FILE
+            fi
+            
         done < "$PACKAGE_LIST"
     else
         echo "${RED}yay is not installed. Please install yay first.${RESET}"
     fi
 elif [[ $ID == "ubuntu" || $ID_LIKE == *"debian"* ]]; then
     #apt update
-    while read -r package; do    
-        sudo apt install -y $package 2>> $LOG_FILE
+    while read -r package; do
+        if command -v $package &>/dev/null;then
+            echo "${YELLOW}[+] $package is already installed!${RESET}"
+        else
+            sudo apt install -y $package 2>> $LOG_FILE
+        fi
+
     done < "$PACKAGE_LIST"    
 else
     echo "${RED}Distribution not supported for automatic package installation!${RESET}"
